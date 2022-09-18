75da93bba31366f1c45c804250d790a0c7f7811c
Jovasa <jovasa46@gmail.com>
2022-09-18 14:06:35 +0300

    legend

--- charter.py
+++ charter.py
@@ -27,18 +27,21 @@ def do_chart():
     for i, t in enumerate(["radical", "kanji", "vocabulary"]):
         ax = fig.add_subplot(311 + i)
         ax.set_title(t.capitalize())
+        labels = []
         for x in range(0, 11):
             f = []
             s = []
             for k, v in data.items():
                 f.append(datetime.datetime.fromisoformat(k))
                 try:
-                    s.append(v[t].get(f"{x}"))
+                    s.append(v[t][f"{x}"])
                 except KeyError:
                     s.append(0)
             if not any(a != 0 for a in s):
                 continue
             ax.plot(f, s, color=colors[x])
+            labels.append(str(x))
+        ax.legend(labels, loc="upper left", ncol=10)
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
     plt.show()
 
